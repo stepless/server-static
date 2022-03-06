78160defa5e114eedccb534e0b62c844f78a2e65
luma <madezhong1220@gmail.com>
2022-03-06 17:49:52 +0800

    init

---
+++ server.js
@@ -0,0 +1,48 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+
+    const filePath = path ==='/' ? 'index.html':path;
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8');
+    //suffix是后缀
+    const suffix = path.substring(path.lastIndexOf('.'));
+    const haxSuffix = {
+      '.html' : 'text/html',
+      '.css' : 'text/css',
+      '.js' : 'text/javascript'
+    }
+    let content;
+    try{
+      content = fs.readFileSync(`public/${filePath}`);
+      response.setHeader('Content-Type', `${haxSuffix[suffix]};charset=utf-8`);
+      response.write(content);
+    }catch{
+      response.statusCode = 404;
+      response.write('文件不存在');
+    }
+    response.end()
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
